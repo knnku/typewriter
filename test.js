f553dbf0021c06c253062139e25139478e43f08f
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-07 04:35:28 +0000

    Refactor code.

---
+++ test.js
@@ -0,0 +1,4 @@
+
+
+// Example usage:
+printStringPerSecond("Hello, World!");
--- typewriter.js
+++ typewriter.js
@@ -1,20 +1,15 @@
 const sentence = "hello there from lighthouse labs";
 
-
-let timer;
 let count = 0;
 
-function printCount(){
-  process.stdout.write(sentence[count]);
-  count++;
-
-
-  //Stop interval when reaching sentence max length and print new line.
-  if (count >= sentence.length) {
-    console.log("\n");
-    clearInterval(timer);
+function printCharacter() {
+  if (count < sentence.length) {
+    process.stdout.write(sentence[count]);
+    count++;
+    setTimeout(printCharacter, 500); 
+    if (count >= sentence.length) {
+      process.stdout.write("\n")
+    }
   }
 }
-
-timer = setInterval(printCount, 100);
-
+printCharacter();
